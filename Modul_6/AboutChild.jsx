9b0d2c07ce432255c658eba3c02cd635d135b0cc
namratasanjaypatil <111340841+namratasanjaypatil@users.noreply.github.com>
2023-10-07 17:03:26 +0530

    Add files via upload

---
+++ Modul_6/AboutChild.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AboutChild() {
+  return (
+    <div className="container-fluid">
+      <p class="placeholder-glow">
+        <span class="placeholder col-12"></span>
+      </p>
+
+      <p class="placeholder-wave">
+        <span class="placeholder col-12"> </span>
+      </p>
+    </div>
+  );
+}
+export default AboutChild;
---
+++ Modul_6/HomeChild.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+function HomeChild() {
+  return (
+    <div className='container'>
+      <div className="row">
+        <h1></h1>
+        <div className="col-md-4">
+          <div className="card" aria-hidden="true">
+            <div className="card-body">
+              <h5 className="card-title placeholder-glow">
+                <span className="placeholder col-6" />
+              </h5>
+              <p className="card-text placeholder-glow">
+                <span className="placeholder col-7" />
+                <span className="placeholder col-4" />
+                <span className="placeholder col-4" />
+              </p>
+              <a className="btn btn-primary disabled placeholder col-6" aria-disabled="true" />
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card" aria-hidden="true">
+            <div className="card-body">
+              <h5 className="card-title placeholder-glow">
+                <span className="placeholder col-6" />
+              </h5>
+              <p className="card-text placeholder-glow">
+                <span className="placeholder col-7" />
+                <span className="placeholder col-4" />
+                <span className="placeholder col-4" />
+              </p>
+              <a className="btn btn-primary disabled placeholder col-6" aria-disabled="true" />
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card" aria-hidden="true">
+            <div className="card-body">
+              <h5 className="card-title placeholder-glow">
+                <span className="placeholder col-6" />
+              </h5>
+              <p className="card-text placeholder-glow">
+                <span className="placeholder col-7" />
+                <span className="placeholder col-4" />
+                <span className="placeholder col-4" />
+              </p>
+              <a className="btn btn-primary disabled placeholder col-6" aria-disabled="true" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+export default HomeChild;
